3985a5c73730fa7b0f099653761ad6c909f60672
Rostislav Král <rostislav-kral@seznam.cz>
2024-04-28 16:16:45 +0200

    TaskController validation for input, postman collection exported

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,16 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Task;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
     public function approximate(Request $request)
     {
-        $start = new \DateTime($request->start);
-        $duration = $request->duration;
-        $ignoreWorkDays = $request->ignoreWorkDays;
-        $startShift = new \DateTime($start->format('Y-m-d').' '.$request->startShift);
-        $endShift = new \DateTime($start->format('Y-m-d').' '.$request->endShift);
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'start' => 'required|date',
+                'duration' => 'required|integer|min:0',
+                'ignoreWorkDays' => 'required|boolean',
+                'startShift' => 'required|date_format:H:i:s',
+                'endShift' => 'required|date_format:H:i:s',
+            ]
+        );
+
+        if ($validator->fails())
+            return response()->json($validator->errors(), 422);
+        $validated = $validator->validate();
+
+        $start = new \DateTime($validated['start']);
+        $duration = $validated['duration'];
+        $ignoreWorkDays = $validated['ignoreWorkDays'];
+        $startShift = new \DateTime($start->format('Y-m-d') . ' ' . $validated['startShift']);
+        $endShift = new \DateTime($start->format('Y-m-d') . ' ' . $validated['endShift']);
 
         $task = new Task($start, $duration, $ignoreWorkDays, $startShift, $endShift);
         $end = $task->approximate();
--- app/Http/Controllers/WorkDayController.php
+++ app/Http/Controllers/WorkDayController.php
@@ -12,13 +12,13 @@ class WorkDayController extends Controller
     public function isWorkDay(Request $request)
     {
         if (!isset($request->date)) {
-            return response('Date is required', 400);
+            return response('Date is required', 422);
         }
 
         try {
             $date = Carbon::parse($request->date)->format('Y/m/d');
         } catch (\Exception $e) {
-            return response('A valid Date is required', 400);
+            return response('A valid Date is required', 422);
 
         }
 
